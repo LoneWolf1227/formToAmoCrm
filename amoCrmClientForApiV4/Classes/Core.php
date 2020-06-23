58ef45bfde7fa6b3b4ba86651d2d9cbd2c02eaa7
LoneWolf1227 <48411716+LoneWolf1227@users.noreply.github.com>
2020-06-23 20:03:36 +0500

    Update Core.php

--- amoCrmClientForApiV4/Classes/Core.php
+++ amoCrmClientForApiV4/Classes/Core.php
@@ -12,9 +12,9 @@ class Core
     public function __construct()
     {
         //Доступ к amoCRM
-        $AMO_DOMAIN = 'manalonewolf';
-        $AMO_API ='7adc591a56a7747af9a257f15be0d32404279948';
-        $AMO_LOGIN ='manalonewolf@gmail.com';
+        $AMO_DOMAIN = '';
+        $AMO_API ='';
+        $AMO_LOGIN ='';
         $this->amo = new libs\hamtimAmocrm($AMO_LOGIN, $AMO_API, $AMO_DOMAIN);
     }
 }
